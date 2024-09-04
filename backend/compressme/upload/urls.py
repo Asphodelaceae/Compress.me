959dc21a6e0555753615617b8a3479012bc36a62
Asphodelaceae <95513065+Asphodelaceae@users.noreply.github.com>
2024-09-03 18:37:24 -0700

    add upload/decrypt endpoint and change url paths to support upload/encrypt and upload/decrypt

--- backend/compressme/upload/urls.py
+++ backend/compressme/upload/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_file_encrypt, name='upload_file_encrypt'),
+    path('encrypt', views.upload_file_encrypt, name='upload_file_encrypt'),
+    path('decrypt', views.upload_file_decrypt, name='upload_file_decrypt')
 ]
